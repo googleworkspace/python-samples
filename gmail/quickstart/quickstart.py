0e321e7d7414c66e8e004f2039d9e383e50fd104
Grant Timmerman <timmerman+devrel@google.com>
2018-04-10 20:18:03 -0700

    Fix gmail start tag

--- gmail/quickstart/quickstart.py
+++ gmail/quickstart/quickstart.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [END gmail_quickstart]
+# [START gmail_quickstart]
 """
 Shows basic usage of the Gmail API.
 
