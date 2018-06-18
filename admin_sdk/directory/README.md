39f40f6c7986bca10afb02a2d3fc5bd3ed1b552e
Grant Timmerman <timmerman+devrel@google.com>
2018-06-18 12:24:23 -0700

    Re-add Admin SDK groups settings quickstart

---
+++ admin_sdk/directory/README.md
@@ -0,0 +1,19 @@
+# Google Admin SDK Directory Python Quickstart
+
+Complete the steps described in the [Google Admin SDK Directory Python
+Quickstart](https://developers.google.com/google-apps/Admin SDK
+Directory/quickstart/python), and in about five minutes you'll have a simple
+Python command-line application that makes requests to the Google Admin SDK
+Directory API.
+
+## Install
+
+```
+pip install --upgrade google-api-python-client
+```
+
+## Run
+
+```
+python quickstart.py
+```
--- admin_sdk/directory/quickstart.py
+++ admin_sdk/directory/quickstart.py
@@ -12,37 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START admin_sdk_groups_settings_quickstart]
+# [START admin_sdk_directory_quickstart]
 """
-Shows basic usage of the Admin SDK Groups Settings API. Outputs a group's
-settings identified by the group's email address.
+Shows basic usage of the Admin SDK Directory API. Lists of first 10 users in the
+domain.
 """
 from __future__ import print_function
-from apiclient.discovery import build, http
+from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import StringIO
-import random
 
-import apiclient
-from email import Utils
-from email import MIMEText
-
-# Setup the Admin SDK Groups Migration API
-SCOPES = 'https://www.googleapis.com/auth/apps.groups.settings'
+# Setup the Admin SDK Directory API
+SCOPES = 'https://www.googleapis.com/auth/admin.directory.user'
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
-service = build('groupssettings', 'v1', http=creds.authorize(Http()))
+service = build('admin', 'directory_v1', http=creds.authorize(Http()))
 
-groupEmail = raw_input('Enter the email address of a Google Group in your domain: ')
-try:
-    results = service.groups().get(groupUniqueId=groupEmail,
-        alt='json').execute()
-    print(json.dumps(results, indent=4))
-except:
-    print('Unable to read group: {0}'.format(groupEmail))
-    raise
-# [END admin_sdk_groups_settings_quickstart]
+# Call the Admin SDK Directory API
+print('Getting the first 10 users in the domain')
+results = service.users().list(customer='my_customer', maxResults=10,
+                               orderBy='email').execute()
+users = results.get('users', [])
+
+if not users:
+    print('No users in the domain.')
+else:
+    print('Users:')
+    for user in users:
+        print('{0} ({1})'.format(user['primaryEmail'], user['name']['fullName']))
+# [END admin_sdk_directory_quickstart]
