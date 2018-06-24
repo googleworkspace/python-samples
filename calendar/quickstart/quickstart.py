afde68c0f8246b53b8328a96f5d606621e6bc15a
tonysepia <electrostep@gmail.com>
2018-06-24 16:47:54 +0100

    updated example
    In the new version the module is called googleapiclient not apiclient

--- calendar/quickstart/quickstart.py
+++ calendar/quickstart/quickstart.py
@@ -18,7 +18,7 @@ Shows basic usage of the Google Calendar API. Creates a Google Calendar API
 service object and outputs a list of the next 10 events on the user's calendar.
 """
 from __future__ import print_function
-from apiclient.discovery import build
+from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import datetime
@@ -33,7 +33,7 @@ if not creds or creds.invalid:
 service = build('calendar', 'v3', http=creds.authorize(Http()))
 
 # Call the Calendar API
-now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
 print('Getting the upcoming 10 events')
 events_result = service.events().list(calendarId='primary', timeMin=now,
                                       maxResults=10, singleEvents=True,
