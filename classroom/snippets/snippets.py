7fac85a647de7afe85a00652b59441bb812acb0d
Lesley <lc2958@columbia.edu>
2018-12-27 09:50:57 -0500

    converted some vars to parameters, fixed indentation

--- classroom/snippets/snippets.py
+++ classroom/snippets/snippets.py
@@ -22,9 +22,7 @@ class ClassroomSnippets(object):
         self.service = service
 
     def create_course(self):
-        """
-        Creates a 10th Grade Biology course.
-        """
+        """ Creates a single Classroom course. """
         service = self.service
         # [START classroom_create_course]
         course = {
@@ -43,13 +41,10 @@ class ClassroomSnippets(object):
         print('Course created:', course.get('name'), course.get('id'))
         # [END classroom_create_course]
 
-    def get_course(self):
-        """
-        Retrieves a classroom course by its id.
-        """
+    def get_course(self, course_id):
+        """ Retrieves a classroom course by its id. """
         service = self.service
         # [START classroom_get_course]
-        course_id = '123456'
         try:
             course = service.courses().get(id=course_id).execute()
             print('Course "{%s}" found.', course.get('name'))
@@ -62,9 +57,7 @@ class ClassroomSnippets(object):
         # [END classroom_get_course]
 
     def list_courses(self):
-        """
-        Lists all classroom courses.
-        """
+        """ Lists all classroom courses. """
         service = self.service
         # [START classroom_list_courses]
         courses = []
@@ -86,13 +79,10 @@ class ClassroomSnippets(object):
                 print(course.get('name'), course.get('id'))
         # [END classroom_list_courses]
 
-    def update_course(self):
-        """
-        Updates the section and room of Google Classroom.
-        """
+    def update_course(self, course_id):
+        """ Updates the section and room of Google Classroom. """
         service = self.service
         # [START classroom_update_course]
-        course_id = '123456'
         course = service.courses().get(id=course_id).execute()
         course['section'] = 'Period 3'
         course['room'] = '302'
@@ -100,13 +90,10 @@ class ClassroomSnippets(object):
         print('Course %s updated.' % course.get('name'))
         # [END classroom_update_course]
 
-    def patch_course(self):
-        """
-        Creates a course with alias specification.
-        """
+    def patch_course(self, course_id):
+        """ Creates a course with alias specification. """
         service = self.service
         # [START classroom_patch_course]
-        course_id = '123456'
         course = {
             'section': 'Period 3',
             'room': '302'
@@ -118,9 +105,7 @@ class ClassroomSnippets(object):
         # [END classroom_patch_course]
 
     def add_alias_new(self):
-        """
-        Creates a course with alias specification.
-        """
+        """ Creates a course with alias specification. """
         service = self.service
         # [START classroom_new_alias]
         alias = 'd:school_math_101'
@@ -139,13 +124,10 @@ class ClassroomSnippets(object):
             print('Course Creation Failed')
         # [END classroom_new_alias]
 
-    def add_alias_existing(self):
-        """
-        Adds alias to existing course.
-        """
+    def add_alias_existing(self, course_id):
+        """ Adds alias to existing course. """
         service = self.service
         # [START classroom_existing_alias]
-        courseId = '123456'
         alias = 'd:school_math_101'
         courseAlias = {
             'alias': alias
@@ -158,209 +140,191 @@ class ClassroomSnippets(object):
             print('Alias Creation Failed')
         # [END classroom_existing_alias]
 
-        def add_teacher(self):
-            """
-            Adds a teacher to a course.
-            """
-            service = self.service
-            # [START classroom_add_teacher]
-            course_id = '123456'
-            teacher_email = 'alice@example.edu'
-            teacher = {
-                'userId': teacher_email
-            }
-            try:
-                teachers = service.courses().teachers()
-                teacher = teachers.create(courseId=course_id,
-                                          body=teacher).execute()
-                print('User %s was added as a teacher to the course with ID %s'
-                      % (teacher.get('profile').get('name').get('fullName'),
-                         course_id))
-            except errors.HttpError as e:
-                error = json.loads(e.content).get('error')
-                if error.get('code') == 409:
-                    print('User "{%s}" is already a member of this course.'
-                          % teacher_email)
-                else:
-                    raise
-            # [END classroom_add_teacher]
-
-        def add_student(self):
-            """
-            Adds a student to a course.
-            """
-            service = self.service
-            # [START classroom_add_student]
-            course_id = '123456'
-            enrollment_code = 'abcdef'
-            student = {
-                'userId': 'me'
-            }
-            try:
-                student = service.courses().students().create(
-                    courseId=course_id,
-                    enrollmentCode=enrollment_code,
-                    body=student).execute()
-                print(
-                    '''User {%s} was enrolled as a student in
-                       the course with ID "{%s}"'''
-                    % (student.get('profile').get('name').get('fullName'),
-                       course_id))
-            except errors.HttpError as e:
-                error = json.loads(e.content).get('error')
-                if error.get('code') == 409:
-                    print('You are already a member of this course.')
-                else:
-                    raise
-            # [END classroom_add_student]
-
-        def create_coursework(self):
-            """
-            Creates a coursework.
-            """
-            service = self.service
-            # [START classroom_create_coursework]
-            courseWork = {
-                'title': 'Ant colonies',
-                'description': '''Read the article about ant colonies
-                                  and complete the quiz.''',
-                'materials': [
-                 {'link': {'url': 'http://example.com/ant-colonies'}},
-                 {'link': {'url': 'http://example.com/ant-quiz'}}
-                ],
-                'workType': 'ASSIGNMENT',
-                'state': 'PUBLISHED',
-            }
-            courseWork = service.courses().courseWork().create(
-                courseId='123456', body=courseWork).execute()
-            print('Assignment created with ID {%s}' % courseWork.get('id'))
-            # [END classroom_create_coursework]
-
-        def list_submissions(self):
-            """
-            Lists all student submissions for a given coursework.
-            """
-            service = self.service
-            # [START classroom_list_submissions]
-            submissions = []
-            page_token = None
-
-            while True:
-                coursework = service.courses().courseWork()
-                response = coursework.studentSubmissions().list(
-                    pageToken=page_token,
-                    courseId="123456",
-                    courseWorkId="654321").execute()
-                submissions.extend(response.get('studentSubmissions', []))
-                page_token = response.get('nextPageToken', None)
-                if not page_token:
-                    break
-
-            if not submissions:
-                print('No student submissions found.')
+    def add_teacher(self, course_id):
+        """ Adds a teacher to a course. """
+        service = self.service
+        # [START classroom_add_teacher]
+        teacher_email = 'alice@example.edu'
+        teacher = {
+            'userId': teacher_email
+        }
+        try:
+            teachers = service.courses().teachers()
+            teacher = teachers.create(courseId=course_id,
+                                      body=teacher).execute()
+            print('User %s was added as a teacher to the course with ID %s'
+                  % (teacher.get('profile').get('name').get('fullName'),
+                     course_id))
+        except errors.HttpError as e:
+            error = json.loads(e.content).get('error')
+            if error.get('code') == 409:
+                print('User "{%s}" is already a member of this course.'
+                      % teacher_email)
             else:
-                print('Student Submissions:')
-                for submission in submissions:
-                    print("%s was submitted at %s" %
-                          (submission.get('id'),
-                           submission.get('creationTime')))
-            # [END classroom_list_submissions]
+                raise
+        # [END classroom_add_teacher]
 
-        def list_student_submissions(self):
-            """
-            Lists all coursework submissions for a given student.
-            """
-            service = self.service
-            # [START classroom_list_student_submissions]
-            submissions = []
-            page_token = None
-
-            while True:
-                coursework = service.courses().courseWork()
-                response = coursework.studentSubmissions().list(
-                    pageToken=page_token,
-                    courseId="123456",
-                    courseWorkId="123456",
-                    userId="123456").execute()
-                submissions.extend(response.get('studentSubmissions', []))
-                page_token = response.get('nextPageToken', None)
-                if not page_token:
-                    break
-
-            if not submissions:
-                print('No student submissions found.')
+    def add_student(self, course_id):
+        """ Adds a student to a course. """
+        service = self.service
+        # [START classroom_add_student]
+        enrollment_code = 'abcdef'
+        student = {
+            'userId': 'me'
+        }
+        try:
+            student = service.courses().students().create(
+                courseId=course_id,
+                enrollmentCode=enrollment_code,
+                body=student).execute()
+            print(
+                '''User {%s} was enrolled as a student in
+                   the course with ID "{%s}"'''
+                % (student.get('profile').get('name').get('fullName'),
+                   course_id))
+        except errors.HttpError as e:
+            error = json.loads(e.content).get('error')
+            if error.get('code') == 409:
+                print('You are already a member of this course.')
             else:
-                print('Student Submissions:')
-                for submission in submissions:
-                    print("%s was submitted at %s" %
-                          (submission.get('id'),
-                           submission.get('creationTime')))
-            # [END classroom_list_student_submissions]
+                raise
+        # [END classroom_add_student]
 
-        def list_all_submissions(self):
-            """
-            Lists all coursework submissions for a given student.
-            """
-            service = self.service
-            # [START classroom_list_submissions]
-            submissions = []
-            page_token = None
+    def create_coursework(self, course_id):
+        """ Creates a coursework. """
+        service = self.service
+        # [START classroom_create_coursework]
+        courseWork = {
+            'title': 'Ant colonies',
+            'description': '''Read the article about ant colonies
+                              and complete the quiz.''',
+            'materials': [
+             {'link': {'url': 'http://example.com/ant-colonies'}},
+             {'link': {'url': 'http://example.com/ant-quiz'}}
+            ],
+            'workType': 'ASSIGNMENT',
+            'state': 'PUBLISHED',
+        }
+        courseWork = service.courses().courseWork().create(
+            courseId=course_id, body=courseWork).execute()
+        print('Assignment created with ID {%s}' % courseWork.get('id'))
+        # [END classroom_create_coursework]
 
-            while True:
-                coursework = service.courses().courseWork()
-                response = coursework.studentSubmissions().list(
-                    pageToken=page_token,
-                    courseId="123456",
-                    courseWorkId="-",
-                    userId="123456").execute()
-                submissions.extend(response.get('studentSubmissions', []))
-                page_token = response.get('nextPageToken', None)
-                if not page_token:
-                    break
+    def list_submissions(self, course_id, coursework_id):
+        """ Lists all student submissions for a given coursework. """
+        service = self.service
+        # [START classroom_list_submissions]
+        submissions = []
+        page_token = None
 
-            if not submissions:
-                print('No student submissions found.')
-            else:
-                print('Complete list of student Submissions:')
-                for submission in submissions:
-                    print("%s was submitted at %s" %
-                          (submission.get('id'),
-                           submission.get('creationTime')))
-            # [END classroom_list_submissions]
-
-        def add_attachment(self):
-            """
-            Adds an attachment to a student submission.
-            """
-            service = self.service
-            # [START classroom_add_attachment]
-            request = {
-              'addAttachments': [
-                {'link': {'url': 'http://example.com/quiz-results'}},
-                {'link': {'url': 'http://example.com/quiz-reading'}}
-              ]
-            }
+        while True:
             coursework = service.courses().courseWork()
-            coursework.studentSubmissions().modifyAttachments(
-                courseId="123456",
-                courseWorkId="123456",
-                id="123456",
-                body=request).execute()
-            # [END classroom_add_attachment]
+            response = coursework.studentSubmissions().list(
+                pageToken=page_token,
+                courseId=course_id,
+                courseWorkId=coursework_id).execute()
+            submissions.extend(response.get('studentSubmissions', []))
+            page_token = response.get('nextPageToken', None)
+            if not page_token:
+                break
 
-        def invite_guardian(self):
-            """
-            Send an invite to a guardian
-            """
-            service = self.service
-            # [START classroom_add_attachment]
-            guardianInvitation = {
-                'invitedEmailAddress': 'guardian@gmail.com',
-            }
-            guardianInvitations = service.userProfiles().guardianInvitations()
-            guardianInvitation = guardianInvitations.create(
-                # You can use a user ID or an email address.
-                studentId='student@mydomain.edu',
-                body=guardianInvitation).execute()
-            print("Invitation created with id: {%s}"
-                  % guardianInvitation.get('invitationId'))
+        if not submissions:
+            print('No student submissions found.')
+        else:
+            print('Student Submissions:')
+            for submission in submissions:
+                print("%s was submitted at %s" %
+                      (submission.get('id'),
+                       submission.get('creationTime')))
+        # [END classroom_list_submissions]
+
+    def list_student_submissions(self, course_id, coursework_id, user_id):
+        """ Lists all coursework submissions for a given student. """
+        service = self.service
+        # [START classroom_list_student_submissions]
+        submissions = []
+        page_token = None
+
+        while True:
+            coursework = service.courses().courseWork()
+            response = coursework.studentSubmissions().list(
+                pageToken=page_token,
+                courseId=course_id,
+                courseWorkId=coursework_id,
+                userId=user_id).execute()
+            submissions.extend(response.get('studentSubmissions', []))
+            page_token = response.get('nextPageToken', None)
+            if not page_token:
+                break
+
+        if not submissions:
+            print('No student submissions found.')
+        else:
+            print('Student Submissions:')
+            for submission in submissions:
+                print("%s was submitted at %s" %
+                      (submission.get('id'),
+                       submission.get('creationTime')))
+        # [END classroom_list_student_submissions]
+
+    def list_all_submissions(self, course_id, user_id):
+        """ Lists all coursework submissions for a given student. """
+        service = self.service
+        # [START classroom_list_submissions]
+        submissions = []
+        page_token = None
+
+        while True:
+            coursework = service.courses().courseWork()
+            response = coursework.studentSubmissions().list(
+                pageToken=page_token,
+                courseId=course_id,
+                courseWorkId="-",
+                userId=user_id).execute()
+            submissions.extend(response.get('studentSubmissions', []))
+            page_token = response.get('nextPageToken', None)
+            if not page_token:
+                break
+
+        if not submissions:
+            print('No student submissions found.')
+        else:
+            print('Complete list of student Submissions:')
+            for submission in submissions:
+                print("%s was submitted at %s" %
+                      (submission.get('id'),
+                       submission.get('creationTime')))
+        # [END classroom_list_submissions]
+
+    def add_attachment(self, course_id, coursework_id, submission_id):
+        """ Adds an attachment to a student submission. """
+        service = self.service
+        # [START classroom_add_attachment]
+        request = {
+          'addAttachments': [
+            {'link': {'url': 'http://example.com/quiz-results'}},
+            {'link': {'url': 'http://example.com/quiz-reading'}}
+          ]
+        }
+        coursework = service.courses().courseWork()
+        coursework.studentSubmissions().modifyAttachments(
+            courseId=course_id,
+            courseWorkId=coursework_id,
+            id=submission_id,
+            body=request).execute()
+        # [END classroom_add_attachment]
+
+    def invite_guardian(self):
+        """ Send an invite to a guardian. """
+        service = self.service
+        # [START classroom_add_attachment]
+        guardianInvitation = {
+            'invitedEmailAddress': 'guardian@gmail.com',
+        }
+        guardianInvitations = service.userProfiles().guardianInvitations()
+        guardianInvitation = guardianInvitations.create(
+            # You can use a user ID or an email address.
+            studentId='student@mydomain.edu',
+            body=guardianInvitation).execute()
+        print("Invitation created with id: {%s}"
+              % guardianInvitation.get('invitationId'))
