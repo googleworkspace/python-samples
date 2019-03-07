17ca53950d743f7ce341a47df2d59da13de142e5
Eric Koleda <ekoleda@google.com>
2019-03-06 21:11:10 -0500

    Change the default port to 8090

--- docs/quickstart/quickstart.py
+++ docs/quickstart/quickstart.py
@@ -44,7 +44,7 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server()
+            creds = flow.run_local_server(port=8090)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
