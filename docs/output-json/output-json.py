5dcaa16bb298a394407958a0860f9332cf27a860
A. W. Fuchs <github@andrewfuchs.com>
2019-04-12 14:50:24 -0700

    Update output-json.py
    Added license and tags

--- docs/output-json/output-json.py
+++ docs/output-json/output-json.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright ©2018-2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+ouput-json.py (Python 2.x or 3.x)
+Google Docs (REST) API output-json sample app
+"""
+# [START output_json_python]
 from __future__ import print_function
 from apiclient import discovery
 from httplib2 import Http
@@ -25,3 +44,4 @@ service = discovery.build('docs', 'v1', http=creds.authorize(
 # Do a document "get" request and print the results as formatted JSON
 result = service.documents().get(documentId=DOCUMENT_ID).execute()
 print(json.dumps(result, indent=4, sort_keys=True))
+# [END output_json_python]
