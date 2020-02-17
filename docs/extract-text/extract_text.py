96123a70e5d8f4c92569f7abba44aab8110493ae
asrivas <asrivastava274@gmail.com>
2020-02-16 23:21:18 -0500

    Format bad spacing

--- docs/extract-text/extract_text.py
+++ docs/extract-text/extract_text.py
@@ -16,8 +16,8 @@
 """Recursively extracts the text from a Google Doc."""
 from __future__ import print_function
 
-from apiclient import discovery
 from httplib2 import Http
+from apiclient import discovery
 from oauth2client import client
 from oauth2client import file
 from oauth2client import tools
@@ -28,7 +28,7 @@ DOCUMENT_ID = 'YOUR_DOCUMENT_ID'
 
 
 def get_credentials():
-  """Gets valid user credentials from storage.
+    """Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
     the OAuth 2.0 flow is completed to obtain the new credentials.
@@ -36,29 +36,29 @@ def get_credentials():
   Returns:
     Credentials, the obtained credential.
   """
-  store = file.Storage('token.json')
-  credentials = store.get()
+    store = file.Storage('token.json')
+    credentials = store.get()
 
-  if not credentials or credentials.invalid:
-    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-    credentials = tools.run_flow(flow, store)
-  return credentials
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        credentials = tools.run_flow(flow, store)
+    return credentials
 
 
 def read_paragraph_element(element):
-  """Returns the text in the given ParagraphElement.
+    """Returns the text in the given ParagraphElement.
 
   Args:
     element: a ParagraphElement from a Google Doc.
   """
-  text_run = element.get('textRun')
-  if not text_run:
-    return ''
-  return text_run.get('content')
+    text_run = element.get('textRun')
+    if not text_run:
+        return ''
+    return text_run.get('content')
 
 
 def read_strucutural_elements(elements):
-  """Recurses through a list of Structural Elements to read a document's text.
+    """Recurses through a list of Structural Elements to read a document's text.
 
      Text may be in nested elements.
 
@@ -68,38 +68,38 @@ def read_strucutural_elements(elements):
   Returns:
     The text as a string.
   """
-  text = ''
-  for value in elements:
-    if 'paragraph' in value:
-      elements = value.get('paragraph').get('elements')
-      for elem in elements:
-        text += read_paragraph_element(elem)
-    elif 'table' in value:
-      # The text in table cells are in nested Structural Elements and tables may
-      # be nested.
-      table = value.get('table')
-      for row in table.get('tableRows'):
-        cells = row.get('tableCells')
-        for cell in cells:
-          text += read_strucutural_elements(cell.get('content'))
-    elif 'tableOfContents' in value:
-      # The text in the TOC is also in a Structural Element.
-      toc = value.get('tableOfContents')
-      text += read_strucutural_elements(toc.get('content'))
-  return text
+    text = ''
+    for value in elements:
+        if 'paragraph' in value:
+            elements = value.get('paragraph').get('elements')
+            for elem in elements:
+                text += read_paragraph_element(elem)
+        elif 'table' in value:
+            # The text in table cells are in nested Structural Elements and
+            # tables may be nested.
+            table = value.get('table')
+            for row in table.get('tableRows'):
+                cells = row.get('tableCells')
+                for cell in cells:
+                    text += read_strucutural_elements(cell.get('content'))
+        elif 'tableOfContents' in value:
+            # The text in the TOC is also in a Structural Element.
+            toc = value.get('tableOfContents')
+            text += read_strucutural_elements(toc.get('content'))
+    return text
 
 
 def main():
-  """Uses the Docs API to print out the text of a document."""
-  credentials = get_credentials()
-  http = credentials.authorize(Http())
-  docs_service = discovery.build(
-      'docs', 'v1', http=http, discoveryServiceUrl=DISCOVERY_DOC)
-  doc = docs_service.documents().get(documentId=DOCUMENT_ID).execute()
-  doc_content = doc.get('body').get('content')
-  print(read_strucutural_elements(doc_content))
+    """Uses the Docs API to print out the text of a document."""
+    credentials = get_credentials()
+    http = credentials.authorize(Http())
+    docs_service = discovery.build(
+        'docs', 'v1', http=http, discoveryServiceUrl=DISCOVERY_DOC)
+    doc = docs_service.documents().get(documentId=DOCUMENT_ID).execute()
+    doc_content = doc.get('body').get('content')
+    print(read_strucutural_elements(doc_content))
 
 
 if __name__ == '__main__':
-  main()
+    main()
 # [END docs_extract_text]
