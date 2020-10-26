a84f3385c95ad80a3a1cd26ec2fb63b499cb738f
Michael Butler <michael@butlerpc.net>
2020-10-25 23:37:05 -0400

    Use a static port number
    Choosing 12997 due to google searches not turning up anything registered for that port.

--- sheets/quickstart/quickstart.py
+++ sheets/quickstart/quickstart.py
@@ -45,7 +45,7 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=12997)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
