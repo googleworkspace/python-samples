e3e3853e1ee80df3628c50d41acdc4e981c64b78
Further <55025025+ifurther@users.noreply.github.com>
2021-11-08 22:47:43 +0800

    change credentials.json to client_secret.json

--- drive/quickstart/quickstart.py
+++ drive/quickstart/quickstart.py
@@ -39,7 +39,7 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'client_secret.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
