590add8be5f5f97d05860d904eab4433199602e1
anuragsamples <anurag@workspacesamples.dev>
2021-12-01 06:19:23 +0000

    Added try except and comments

--- gmail/quickstart/quickstart.py
+++ gmail/quickstart/quickstart.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 # [START gmail_quickstart]
+
 from __future__ import print_function
 import os.path
 from googleapiclient.discovery import build
@@ -23,40 +24,50 @@ from google.oauth2.credentials import Credentials
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
+
 def main():
     """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
+        Lists the user's Gmail labels.
     """
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    try:
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                       'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
 
-    service = build('gmail', 'v1', credentials=creds)
+        service = build('gmail', 'v1', credentials=creds)
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+        # Call the Gmail API
+        results = service.users().labels().list(userId='me').execute()
+        labels = results.get('labels', [])
+
+    # Handle the exception
+    except Exception as err:
+        print(err)
 
     if not labels:
         print('No labels found.')
     else:
         print('Labels:')
         for label in labels:
+            # Print labels' name
             print(label['name'])
+
+
+
 
 if __name__ == '__main__':
     main()
