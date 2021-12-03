72ce601ea7bd3762405e444632046bac9b18b125
Steven Bazyl <sbazyl@google.com>
2021-12-03 10:46:39 -0700

    Only run tests when internal PR or after push

--- .github/scripts/lint.sh
+++ .github/scripts/lint.sh
@@ -18,8 +18,9 @@ set -e
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-if [ -f "${HOME}/changed_files.txt" ]; then
-  # TODO - Check for .pylintrc changes
+CL_FILE="${HOME}/changed_files.txt"
+
+if [ -f "${CL_FILE}" ] && ! grep -q .pylintrc "${CL_FILE}"; then
   cat "${HOME}/changed_files.txt" | grep ".py$" | xargs pylint
 else
   find . -iname "*.py" | xargs pylint
