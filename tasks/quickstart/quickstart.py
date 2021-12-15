432ad7e0abaa5fc5365861c2598ac94f39d2225b
Steve Bazyl <sqrrrl@gmail.com>
2021-12-15 11:30:43 -0700

    Update quickstart.py

--- tasks/quickstart/quickstart.py
+++ tasks/quickstart/quickstart.py
@@ -21,6 +21,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/tasks.readonly']
@@ -34,21 +35,21 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    try:
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
+    try:
         service = build('tasks', 'v1', credentials=creds)
 
         # Call the Tasks API
@@ -57,11 +58,12 @@ def main():
 
         if not items:
             print('No task lists found.')
-        else:
-            print('Task lists:')
-            for item in items:
-                print(u'{0} ({1})'.format(item['title'], item['id']))
-    except Exception as err:
+            return
+
+        print('Task lists:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['title'], item['id']))
+    except HttpError as err:
         print(err)
 
 
