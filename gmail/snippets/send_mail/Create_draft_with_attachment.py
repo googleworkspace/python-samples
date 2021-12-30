bfd944197cd7e772f8e36d65516f178795400108
anuraggoogler <aanursharm@google.com>
2021-12-30 10:15:52 +0000

    Implemented Create_draft_with_attachment

---
+++ gmail/snippets/send_mail/Create_draft_with_attachment.py
@@ -0,0 +1,116 @@
+from __future__ import print_function
+
+import mimetypes
+import os
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import base64
+from email.mime.text import MIMEText
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
+
+
+# [START gmail_create_draft_with_attachment]
+def gmailCreateDraftWithAttachment():
+    """Create and insert a draft email with attachment. Print the returned draft's message and id.
+      Returns: Draft object, including draft id and message meta data.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+
+        sender = 'gduser1@workspacesamples.dev'
+        to = 'gduser2@workspacesamples.dev'
+        subject = 'sample with attachment'
+        message_body = 'Hi, this is automated mail with attachment.Please do not reply '
+        file_name = 'photo.jpg'
+        message = {'message': create_message(sender=sender, to=to, subject=subject, message_text=message_body,
+                                             file=file_name)}
+        draft = service.users().drafts().create(userId="me", body=message).execute()
+
+        print('Draft id: %s\nDraft message: %s' % (draft['id'], draft['message']))
+        return draft
+
+    except Exception as error:
+        print('An error occurred: %s' % error)
+
+
+def create_message(sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+      file: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(file, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+
+if __name__ == '__main__':
+    gmailCreateDraftWithAttachment()
+# [START gmail_create_draft_with_attachment]
+
