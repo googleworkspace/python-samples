ac52d80f15c5baedea8bc9ce920d2d2eb1051e79
anuraggoogler <aanursharm@google.com>
2021-12-31 09:33:20 +0000

    Implemented send_mail snippet

---
+++ gmail/snippets/send_mail/send_mail.py
@@ -0,0 +1,183 @@
+from __future__ import print_function
+
+import mimetypes
+import os
+from email.mime.text import MIMEText
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+from googleapiclient import errors
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import base64
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
+
+
+def main():
+    """Create and insert a draft email. Print the returned draft's message and id.
+      Returns: Draft object, including draft id and message meta data.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+
+        user_id = 'gduser1@workspacesamples.dev'
+        sender_id = 'gduser2@workspacesamples.dev'
+        subject = 'Automated message'
+        message_body = 'This is automated draft mail'
+        file_name= 'photo.jpg'
+        message = create_message(sender=sender_id, to=user_id, subject=subject, message_text=message_body)
+        message_with_attachment = create_message_with_attachment(sender=sender_id, to=user_id, subject=subject,
+                                                                message_text=message_body, file=file_name)
+
+        create_draft(service=service, user_id="me", message_body=message)
+        send_message(service=service, user_id="me", message=message)
+        create_draft(service=service, user_id="me", message_body=message_with_attachment)
+        send_message(service=service, user_id="me", message=message_with_attachment)
+
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+# [START create_draft]
+def create_draft(service, user_id, message_body):
+    """Create and insert a draft email. Print the returned draft's message and id.
+
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me" can be used to indicate the authenticated user.
+        message_body: The body of the email message, including headers.
+
+    Returns:
+        Draft object, including draft id and message meta data.
+    """
+    try:
+        message = {'message': message_body}
+        draft = service.users().drafts().create(userId=user_id, body=message).execute()
+
+        print('Draft id: %s\nDraft message: %s' % (draft['id'], draft['message']))
+
+        return draft
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+# [END create_draft]
+
+
+# [START send_message]
+def send_message(service, user_id, message):
+    """Send an email message.
+
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me" can be used to indicate the authenticated user.
+        message: Message to be sent.
+
+    Returns:
+        Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+# [END send_email]
+
+
+# [START create_message]
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+    Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+# [END create_message]
+
+
+# [START create_message_attachment]
+def create_message_with_attachment(sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+      file: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text)
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(file, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+# [END create_message_attachment]
+
+
+if __name__ == '__main__':
+    main()
