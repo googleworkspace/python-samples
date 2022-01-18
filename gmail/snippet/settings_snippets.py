d22e47e38212ecc139ec0eebc2f6e679d44c1a38
Rajesh Mudaliyar <mudaliyar@google.com>
2022-01-18 10:45:02 -0800

    Gmail snippets original code from devral repo. (#248)
    Import gmail snippets from internal repo
    
    Co-authored-by: Steve Bazyl <sqrrrl@gmail.com>
    Co-authored-by: anuraggoogler <aanursharm@google.com>

---
+++ gmail/snippet/settings_snippets.py
@@ -0,0 +1,96 @@
+from datetime import datetime, timedelta
+from numpy import long
+
+
+class SettingsSnippets:
+
+    def __init__(self, service):
+        self.service = service
+
+    def update_signature(self):
+        gmail_service = self.service
+        # [START updateSignature]
+        primary_alias = None
+        aliases = gmail_service.users().settings().sendAs().\
+            list(userId='me').execute()
+        for alias in aliases.get('sendAs'):
+            if alias.get('isPrimary'):
+                primary_alias = alias
+                break
+
+        sendAsConfiguration = {
+            'signature': 'I heart cats'
+        }
+        result = gmail_service.users().settings().sendAs().\
+            patch(userId='me',
+                  sendAsEmail=primary_alias.get('sendAsEmail'),
+                  body=sendAsConfiguration).execute()
+        print('Updated signature for: %s' % result.get('displayName'))
+        # [END updateSignature]
+        return result.get('signature')
+
+    def create_filter(self, real_label_id):
+        gmail_service = self.service
+        # [START createFilter]
+        label_id = 'Label_14' # ID of user label to add
+        # [START_EXCLUDE silent]
+        label_id = real_label_id
+        # [END_EXCLUDE]
+        filter = {
+            'criteria': {
+                'from': 'cat-enthusiasts@example.com'
+            },
+            'action': {
+                'addLabelIds': [label_id],
+                'removeLabelIds': ['INBOX']
+            }
+        }
+        result = gmail_service.users().settings().filters().\
+            create(userId='me', body=filter).execute()
+        print('Created filter: %s' % result.get('id'))
+        # [END createFilter]
+        return result.get('id')
+
+    def enable_forwarding(self, real_forwarding_address):
+        gmail_service = self.service
+        # [START enableForwarding]
+        address = { 'forwardingEmail': 'user2@example.com' }
+        # [START_EXCLUDE silent]
+        address = { 'forwardingEmail': real_forwarding_address }
+        # [END_EXCLUDE]
+        result = gmail_service.users().settings().forwardingAddresses().\
+            create(userId='me', body=address).execute()
+        if result.get('verificationStatus') == 'accepted':
+            body = {
+                'emailAddress': result.get('forwardingEmail'),
+                'enabled': True,
+                'disposition': 'trash'
+            }
+            result = gmail_service.users().settings().\
+                updateAutoForwarding(userId='me', body=body).execute()
+            # [START_EXCLUDE silent]
+            return result
+            # [END_EXCLUDE]
+
+        # [END enableForwarding]
+        return None
+
+    def enable_auto_reply(self):
+        gmail_service = self.service
+        # [START enableAutoReply]
+        epoch = datetime.utcfromtimestamp(0)
+        now = datetime.now()
+        start_time = (now - epoch).total_seconds() * 1000
+        end_time = (now + timedelta(days=7) - epoch).total_seconds() * 1000
+        vacation_settings = {
+            'enableAutoReply': True,
+            'responseBodyHtml': "I'm on vacation and will reply when I'm "
+                                "back in the office. Thanks!",
+            'restrictToDomain': True,
+            'startTime': long(start_time),
+            'endTime': long(end_time)
+        }
+        response = gmail_service.users().settings().\
+            updateVacation(userId='me', body=vacation_settings).execute()
+        # [END enableAutoReply]
+        return response
---
+++ gmail/snippet/test_send_mail.py
@@ -0,0 +1,55 @@
+import unittest
+
+import send_mail
+from base_test import BaseTest
+
+
+
+class SendMailTest(BaseTest):
+    @classmethod
+    def setUpClass(cls):
+        super(SendMailTest, cls).setUpClass()
+
+    def setUp(self):
+        super(SendMailTest, self).setUp()
+
+    def tearDown(self):
+        super(SendMailTest, self).tearDown()
+
+
+    def test_create_message(self):
+        message = send_mail.create_message(SendMailTest.TEST_USER,
+                                           SendMailTest.RECIPIENT,
+                                           'Test',
+                                           'Hello!')
+        self.assertIsNotNone(message) # Weak assertion
+
+    def test_create_message_with_attachment(self):
+        message = send_mail.create_message_with_attachment(SendMailTest.TEST_USER,
+                                                           SendMailTest.RECIPIENT,
+                                                           'Test',
+                                                           'Hello!',
+                                                           'files/photo.jpg')
+        self.assertIsNotNone(message) # Weak assertion
+
+    def test_create_draft(self):
+        message = send_mail.create_message(SendMailTest.TEST_USER,
+                                           SendMailTest.RECIPIENT,
+                                           'Test',
+                                           'Hello!')
+        draft = send_mail.create_draft(self.service, 'me', message)
+        self.assertIsNotNone(draft)
+        self.service.users().drafts().delete(userId='me', id=draft.get('id'))
+
+    def test_send_mail(self):
+        message = send_mail.create_message_with_attachment(SendMailTest.TEST_USER,
+                                                           SendMailTest.RECIPIENT,
+                                                           'Test',
+                                                           'Hello!',
+                                                           'files/photo.jpg')
+        sent_message = send_mail.send_message(self.service, 'me', message)
+        self.assertIsNotNone(sent_message)
+
+
+if __name__ == '__main__':
+    unittest.main()
