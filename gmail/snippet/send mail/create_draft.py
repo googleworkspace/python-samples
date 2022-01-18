4cf1c0571ba96f8583d600600334a2051c518459
anuraggoogler <aanursharm@google.com>
2022-01-18 08:49:54 +0000

    Create and insert a draft email with and without attachment

--- gmail/snippet/send mail/create_draft.py
+++ gmail/snippet/send mail/create_draft.py
@@ -14,44 +14,32 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 """
-
 # [START gmail_create_draft]
 
 from __future__ import print_function
+
 import base64
-import os
 from email.mime.text import MIMEText
+
+import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-
-SCOPES = ['https://www.googleapis.com/auth/gmail.compose']
 
 
 def gmail_create_draft():
     """Create and insert a draft email.
        Print the returned draft's message and id.
-      Returns: Draft object, including draft id and message meta data.
+       Returns: Draft object, including draft id and message meta data.
+
+      Load pre-authorized user credentials from the environment.
+      TODO(developer) - See https://developers.google.com/identity
+      for guides on implementing OAuth2 for the application.
     """
-    cred = None
-    if os.path.exists('token.json'):
-        cred = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not cred or not cred.valid:
-        if cred and cred.expired and cred.refresh_token:
-            cred.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            cred = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w', encoding='UTF') as token:
-            token.write(cred.to_json())
+    creds, _ = google.auth.default()
 
     try:
         # create gmail api client
-        service = build('gmail', 'v1', credentials=cred)
+        service = build('gmail', 'v1', credentials=creds)
 
         message = MIMEText('This is automated draft mail')
         message['to'] = 'gduser1@workspacesamples.dev'
