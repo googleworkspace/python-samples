5b67c7c9f4275ff4564da2aa11ccfea0a89e0b8c
Rajesh Mudaliyar <mudaliyar@google.com>
2022-02-15 12:25:11 -0800

    Created classroom_create_coursework.py to the classroom snippets API (#275)
    * Added pull request template
    
    Added pull request template
    
    * updated header
    
    updated header
    
    * Update pull_request_template.md
    
    * Update pull_request_template.md
    
    * Add unit/integration test to checklist
    
    * Gmail snippets original code from devral repo.
    
    * Gmail snippets original code from devral repo.
    
    * Create and send an email message with and without attachment
    
    * Update send_message.py
    
    * Update send_message_with_attachment.py
    
    * Update send_message.py
    
    * Delete send_message.py
    
    * Delete send_message_with_attachment.py
    
    * Added 4 files in classroom snippet.
    
    * Adds a teacher to a course.
    
    * user ID changed
    
    user ID changed
    
    * Made changes to the classroom snippets files as per review.
    
    * Added classroom_patch_course.py file to the classroom snippets API.
    
    * Made changes to the classroom snippets files as per review.
    
    * Added files to the classroom snippets API.
    
    * Delint  imports + adjust region tag
    
    * Delint imports + adjust region tag
    
    * Delint imports + adjust region tag
    
    * Delint imports + adjust region tag
    
    * Remove blank line at start of region tag
    
    * Delint - remove extra blank line
    
    * Created classroom_create_coursework.py to the classroom snippets API.
    
    * Created 2 new files and, as per review made changes to 1 file in classroom snippets API .
    
    * Made changes to the classroom snippets files as per review.
    
    * Created a file to get course details in classroom snippets API.
    
    Co-authored-by: Steve Bazyl <sqrrrl@gmail.com>
    Co-authored-by: anuraggoogler <aanursharm@google.com>
    Co-authored-by: himanshupr2627 <himanshupr@google.com>

---
+++ classroom/snippets/classroom_create_coursework.py
@@ -0,0 +1,65 @@
+"""
+Copyright 2022 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+from __future__ import print_function
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# [START classroom_create_coursework]
+
+
+def classroom_create_coursework(course_id):
+
+    """
+    Creates the coursework the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.\n"
+    """
+
+    creds, _ = google.auth.default()
+    # pylint: disable=maybe-no-member
+
+    try:
+        service = build('classroom', 'v1', credentials=creds)
+        coursework = {
+            'title': 'Ant colonies',
+            'description': '''Read the article about ant colonies
+                              and complete the quiz.''',
+            'materials': [
+                {'link': {'url': 'http://example.com/ant-colonies'}},
+                {'link': {'url': 'http://example.com/ant-quiz'}}
+            ],
+            'workType': 'ASSIGNMENT',
+            'state': 'PUBLISHED',
+        }
+        coursework = service.courses().courseWork().create(
+            courseId=course_id, body=coursework).execute()
+        print(f"Assignment created with ID {coursework.get('id')}")
+        return coursework
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+
+if __name__ == '__main__':
+    # Put the course_id of course whose coursework needs to be created,
+    # the user has access to.
+    classroom_create_coursework(453686957652)
+# [END classroom_create_coursework]
