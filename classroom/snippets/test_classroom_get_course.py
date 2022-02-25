532854bc1851089d3391f3bd2008e98b3c2978c1
himanshupr2627 <himanshupr@google.com>
2022-02-25 20:10:45 +0530

    Update test_classroom_get_course.py

--- classroom/snippets/test_classroom_get_course.py
+++ classroom/snippets/test_classroom_get_course.py
@@ -1,23 +1,32 @@
-"""Unit test file for Get course snippet"""
+"""
+Copyright 2022 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
 import unittest
+
 import classroom_get_course
 import classroom_list_courses
 
-"""
-Unit test class for Get course snippet
-"""
 
-
-class TestClassroomGetCourse(unittest.TestCase):
-    """Unit test class for Get course snippet"""
+"""Unit test class for Get course snippet"""
+class TestClassroomGetCourse(unittest.TestCase):   
     @classmethod
     def test_classroom_get_course(cls):
+        """Unit test method for Get course snippet"""
         course = classroom_list_courses.classroom_list_courses()
-        for i in course:
-            course_id = classroom_get_course.classroom_get_course(course.
-                                                                  get('id'))
-            cls.assertIsNotNone(cls, course_id)
-            break
+        course_id = classroom_get_course.classroom_get_course(course.get(
+                    'id'))
+        cls.assertIsNotNone(cls, course_id)
 
 
 if __name__ == "__main__":
