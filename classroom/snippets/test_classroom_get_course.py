99ae4d1590d1d07bd0ecf45eed583637381752de
Rajesh Mudaliyar <mudaliyar@google.com>
2022-03-02 14:51:28 -0800

    Created unit test case file for classroom snippet API. (#292)
    * Created unit test case file for classroom snippet API.
    
    * Update test_classroom_create_course.py
    
    * Update test_classroom_create_course.py
    
    * Update test_classroom_list_courses.py
    
    * Update test_classroom_get_course.py
    
    * Update test_classroom_get_course.py
    
    * Created unit test case file for classroom snippet API.
    
    Co-authored-by: himanshupr2627 <himanshupr@google.com>

---
+++ classroom/snippets/test_classroom_get_course.py
@@ -0,0 +1,33 @@
+"""
+Copyright 2022 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+import unittest
+
+import classroom_get_course
+import classroom_list_courses
+
+
+class TestClassroomGetCourse(unittest.TestCase):
+    """Unit test class for Get course snippet"""
+    @classmethod
+    def test_classroom_get_course(cls):
+        """Unit test method for Get course snippet"""
+        course = classroom_list_courses.classroom_list_courses()
+        course_id = classroom_get_course.classroom_get_course(course.get(
+                    'id'))
+        cls.assertIsNotNone(cls, course_id)
+
+
+if __name__ == "__main__":
+    unittest.main()
