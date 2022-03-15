29ae92ef31050764fd6d902b3b318f185a65166e
Rajesh Mudaliyar <mudaliyar@google.com>
2022-03-15 15:41:36 -0700

    Created files for Slides snippets (#297)
    * Created files for Slides snippets.
    
    * Created files for Slides snippets.
    
    * Created files for Slides snippets.
    
    Co-authored-by: himanshupr2627 <himanshupr@google.com>

---
+++ slides/snippets/slides_create_presentation.py
@@ -0,0 +1,58 @@
+"""
+Copyright 2022 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# [START slides_create_presentation]
+from __future__ import print_function
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def create_presentation(title):
+    """
+        Creates the Presentation the user has access to.
+        Load pre-authorized user credentials from the environment.
+        TODO(developer) - See https://developers.google.com/identity
+        for guides on implementing OAuth2 for the application.\n"
+        """
+    creds, _ = google.auth.default()
+    # pylint: disable=maybe-no-member
+    try:
+        service = build('slides', 'v1', credentials=creds)
+
+        body = {
+            'title': title
+        }
+        presentation = service.presentations() \
+            .create(body=body).execute()
+        print(f"Created presentation with ID:"
+              f"{(presentation.get('presentationId'))}")
+        # [END slides_create_presentation]
+        return presentation
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        print("presentation not created")
+        return error
+
+
+if __name__ == '__main__':
+    # Put the title of the presentation
+
+    create_presentation("finalp")
+
+# [END slides_create_presentation]
---
+++ slides/snippets/slides_create_slide.py
@@ -0,0 +1,75 @@
+"""
+Copyright 2022 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# [START slides_create_slide]
+from __future__ import print_function
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def create_slide(presentation_id, page_id):
+    """
+    Creates the Presentation the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.\n"
+    """
+    creds, _ = google.auth.default()
+    # pylint: disable=maybe-no-member
+    try:
+        service = build('slides', 'v1', credentials=creds)
+        # [START slides_create_slide]
+        # Add a slide at index 1 using the predefined
+        # 'TITLE_AND_TWO_COLUMNS' layout and the ID page_id.
+        requests = [
+            {
+                'createSlide': {
+                    'objectId': page_id,
+                    'insertionIndex': '1',
+                    'slideLayoutReference': {
+                        'predefinedLayout': 'TITLE_AND_TWO_COLUMNS'
+                    }
+                }
+            }
+        ]
+
+        # If you wish to populate the slide with elements,
+        # add element create requests here, using the page_id.
+
+        # Execute the request.
+        body = {
+            'requests': requests
+        }
+        response = service.presentations() \
+            .batchUpdate(presentationId=presentation_id, body=body).execute()
+        create_slide_response = response.get('replies')[0].get('createSlide')
+        print(f"Created slide with ID:"
+              f"{(create_slide_response.get('objectId'))}")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        print("Slides not created")
+        return error
+
+    return response
+
+
+if __name__ == '__main__':
+    # Put the presentation_id, Page_id of slides whose list needs
+    # to be submitted.
+    create_slide("12SQU9Ik-ShXecJoMtT-LlNwEPiFR7AadnxV2KiBXCnE", "My4ndpage")
+    # [END slides_create_slide]
