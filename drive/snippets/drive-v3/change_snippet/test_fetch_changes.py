953ad6b0624a6bcfe078ed4691a220c64a9d4a7c
Rajesh Mudaliyar <mudaliyar@google.com>
2022-05-31 15:15:45 -0700

    UT for 3 region tags for app_data_snippet and change_snippet folder of drive-v3. (#315)
    * UT for 3 region tags for app_data_snippet for drive-v3.
    
    * UT for 3 region tags for change_snippet for drive-v3.
    
    * Delete test_fetch_changes.py
    
    * Delete test_fetch_start_page_token.py
    
    * UT for 2 region tags for change_snippet for drive-v3.
    
    Co-authored-by: himanshupr <himanshupr@google.com>

---
+++ drive/snippets/drive-v3/change_snippet/test_fetch_changes.py
@@ -0,0 +1,34 @@
+"""Copyright 2022 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+
+import fetch_changes
+import fetch_start_page_token
+
+
+class TestFetchChanges(unittest.TestCase):
+    """Unit test classs for Change snippet"""
+
+    @classmethod
+    def test_fetch_changes(cls):
+        """Test fetch_changes"""
+        start_token = fetch_start_page_token.fetch_start_page_token()
+        token = fetch_changes.fetch_changes(start_token)
+        cls.assertIsNotNone(cls, token)
+
+
+if __name__ == '__main__':
+    unittest.main()
