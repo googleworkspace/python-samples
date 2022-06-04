f68daebe349d97309af34bfaad5531aab986a1c3
lecovi <lecovi@ac.python.org.ar>
2022-06-04 16:56:08 -0300

    fix: updates Python email API at send_mail.py example

--- gmail/snippet/send mail/send_message.py
+++ gmail/snippet/send mail/send_message.py
@@ -15,7 +15,7 @@ limitations under the License.
 from __future__ import print_function
 
 import base64
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 
 import google.auth
 from googleapiclient.discovery import build
@@ -35,7 +35,8 @@ def gmail_send_message():
 
     try:
         service = build('gmail', 'v1', credentials=creds)
-        message = MIMEText('This is automated draft mail')
+        message = EmailMessage()
+        message.set_content('This is automated draft mail')
         message['To'] = 'gduser1@workspacesamples.dev'
         message['From'] = 'gduser2@workspacesamples.dev'
         message['Subject'] = 'Automated draft'
