8f879da6907cd682eba5d2ad8fed1879e0eab1f0
suchawla09 <suchawla@google.com>
2022-06-20 15:10:24 +0530

    Slides UT Added

---
+++ slides/snippets/test_slides_create_sheets_chart.py
@@ -0,0 +1,42 @@
+"""
+Copyright 2022 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+from pprint import pformat
+
+import slides_create_sheets_chart
+from base_test import BaseTest
+
+
+class TestCreateSheetsChart(BaseTest):
+    """Unit test for create_sheets_chart snippet"""
+    DATA_SPREADSHEET_ID = '17eqFZl_WK4WVixX8PjvjfLD77DraoFwMDXeiHB3dvuM'
+    CHART_ID = 1107320627
+
+    def test_create_sheets_chart(self):
+        """create_sheets_chart method """
+        presentation_id = self.create_test_presentation()
+        page_id = self.add_slides(presentation_id, 1, 'BLANK')[0]
+        response = slides_create_sheets_chart. \
+            create_sheets_chart(presentation_id, page_id,
+                                self.DATA_SPREADSHEET_ID,
+                                self.CHART_ID)
+        self.assertEqual(1, len(response.get('replies')),
+                         msg=pformat(response))
+        chart_id = response.get('replies')[0].get('createSheetsChart') \
+            .get('objectId')
+        self.assertIsNotNone(chart_id, msg=pformat(response))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ slides/snippets/test_slides_simple_text_replace.py
@@ -0,0 +1,36 @@
+"""
+Copyright 2022 Google LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+from pprint import pformat
+
+import slides_simple_text_replace
+from base_test import BaseTest
+
+
+class TestSimpleTextReplace(BaseTest):
+    """Unit test for SimpleTextReplace snippet"""
+
+    def test_simple_text_replace(self):
+        """test_simple_text_replace function"""
+        presentation_id = self.create_test_presentation()
+        page_id = self.add_slides(presentation_id, 1, 'BLANK')[0]
+        box_id = self.create_test_textbox(presentation_id, page_id)
+        response = slides_simple_text_replace.simple_text_replace(
+            presentation_id, box_id, 'MY NEW TEXT')
+        self.assertEqual(2, len(response.get('replies')),
+                         msg=pformat(response))
+
+
+if __name__ == "__main__":
+    unittest.main()
